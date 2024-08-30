a792385d232886f61b62816f6e2badf02b8b2e07
gabriel <contato.gabrielfeitosa@gmail.com>
2024-08-30 12:12:15 -0500

    added interface for Measurement model

--- src/models/Measurement.ts
+++ src/models/Measurement.ts
@@ -1,17 +1,28 @@
-import mongoose from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
 import { v4 as uuidv4 } from "uuid";
 
-const measurementSchema = new mongoose.Schema({
+interface IMeasurement extends Document {
+  _id: string;
+  measure_uuid: string;
+  measure_datetime: Date;
+  measure_type: 'WATER' | 'GAS';
+  measure_value: number;
+  has_confirmed: boolean;
+  customer_code: string;
+  image_url: string;
+}
+
+const measurementSchema: Schema<IMeasurement> = new mongoose.Schema({
   _id: { type: String, default: uuidv4 },
   measure_uuid: { type: String, default: uuidv4, unique: true },
   measure_datetime: { type: Date, required: true },
   measure_type: { type: String, enum: ["WATER", "GAS"], required: true },
   measure_value: { type: Number, required: true },
-  has_confirmed: { type: Boolean, default: false, require: true },
+  has_confirmed: { type: Boolean, default: false, required: true },
   customer_code: { type: String, required: true },
   image_url: { type: String, required: true },
 });
 
-const Measurement = mongoose.model("Measurement", measurementSchema);
+const Measurement = mongoose.model<IMeasurement>("Measurement", measurementSchema);
 
 export default Measurement;
