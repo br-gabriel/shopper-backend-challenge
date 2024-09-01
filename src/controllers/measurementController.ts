67ebd265296de8b3926e38b3b9671024b2760e6b
gabriel <contato.gabrielfeitosa@gmail.com>
2024-09-01 17:09:59 -0500

    controllers created

--- src/controllers/measurementController.ts
+++ src/controllers/measurementController.ts
@@ -2,64 +2,79 @@ import dotenv from "dotenv";
 import Measurement from "../models/Measurement";
 import { Request, Response } from "express";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { fileTypeFromBuffer } from "file-type";
 
 dotenv.config();
 
 const uploadMeasurement = async(req: Request, res: Response) => {
-    const { image, customer_code, measure_datetime, measure_type } = req.body;
+    const { image, customer_code, measure_type, measure_datetime } = req.body;
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
-    
-    const validMimeTypes = ['image/png', 'image/jpeg', 'image/jpg', 'image/webp'];
-
-    const detectMimeTypeFromBase64 = (base64: string): string | undefined => {
-        if (base64.startsWith('iVBORw0KGgo')) return 'image/png';
-        if (base64.startsWith('/9j/')) return 'image/jpeg';
-        if (base64.startsWith('UklGR')) return 'image/webp';
-        return undefined;
-      };
-
-    const extractNumbers = (response: string): number[] => {
-        const numberPattern = /\d+/g;
-        return (response.match(numberPattern) || []).map(Number);
-    };
 
     try {
-        const model = await genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-        const prompt = `Analise a imagem fornecida de um hidrômetro e extraia apenas os números de cor preta. Por favor, ignore e não inclua na resposta quaisquer números que estejam na cor vermelha. Retorne somente os números pretos visíveis na imagem`;
-
-        const mimeType = detectMimeTypeFromBase64(image);
-
-        if (!mimeType || !validMimeTypes.includes(mimeType)) {
-            return res.status(400).json({ message: 'Tipo MIME não suportado ou não detectado.' });
+        if (!image || !customer_code || !measure_type || !measure_datetime) {
+            return res.status(400).json({
+              error_code: "INVALID_DATA",
+              error_description: "Os dados fornecidos no corpo da requisição são inválidos",
+            });
         }
 
-        const imageData = {
+        const measureDate = new Date(measure_datetime);
+
+        const existingMeasurement = await Measurement.findOne({
+            customer_code,
+            measure_type,
+            $expr: {
+              $and: [
+                { $eq: [{ $month: "$measure_datetime" }, measureDate.getMonth() + 1] },
+                { $eq: [{ $year: "$measure_datetime" }, measureDate.getFullYear()] }
+              ]
+            }
+        } as any);
+
+        if (existingMeasurement) {
+            return res.status(409).json({
+              error_code: "DOUBLE_REPORT",
+              error_description: "Leitura do mês já realizada",
+            });
+        }
+
+
+        const model = await genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const prompt = `Leia a imagem e me retorne apenas os números dentro do registro (o registro é a área retangular com dígitos dentro, o valor a ser lido é apenas esses), trazendo apenas os números como caracteres`;
+
+        const buffer: Buffer = Buffer.from(image, 'base64');
+        const fileInfo = await fileTypeFromBuffer(buffer);
+        
+        if(!fileInfo) {
+            return res.status(400).json({ message: "Tipo de arquivo não suportado" })
+        };
+        
+        const mimeType = fileInfo.mime;
+
+        const imageData  = {
             inlineData: {
-              data: image,
-              mimeType: mimeType,
-            },
-          };
+                data: image,
+                mimeType: mimeType,
+            }
+        };
 
-        const result = await model.generateContent([prompt, imageData]);
+        const result = await model.generateContent([imageData, prompt]);
         const responseText = await result.response.text();
+        const value = parseInt(responseText);
 
-        const extractedValue = extractNumbers(responseText);
+        const fakeUrlImage = "https://example.com/YHfbdomsWEx";
 
-        const newMeasurement = new Measurement({
-            image_url: 'URL da Imagem',
-            measure_time: measure_datetime,
+        const measurement = new Measurement({
+            measure_datetime: measure_datetime,
             measure_type: measure_type,
+            measure_value: value,
             customer_code: customer_code,
-            measure_value: extractedValue[0],
+            image_url: fakeUrlImage,
         });
 
-        const savedMeasurement = await newMeasurement.save();
+        await measurement.save();
 
-        res.status(200).json({
-            image_url: 'URL da Imagem',
-            id: savedMeasurement._id,
-            measure_value: extractedValue[0],
-        });
+        res.status(200).json({ image_url: fakeUrlImage, measure_value: value, measure_uuid: measurement._id });
     } catch (err: any) {
         res
         .status(500)
@@ -67,27 +82,91 @@ const uploadMeasurement = async(req: Request, res: Response) => {
     }
 };
 
-// const confirmMeasurement = async(req: Request, res: Response) => {
-//   try {
-//   } catch (err: any) {
-//     res
-//       .status(500)
-//       .json({ message: "Erro interno do servidor", error: err.message });
-//   }
-// };
+const confirmMeasurement = async(req: Request, res: Response) => {
+  const { measure_uuid, confirmed_value } = req.body;
+  
+  try {
+    if (!measure_uuid || isNaN(confirmed_value)) {
+        return res.status(400).json({
+            error_code: "INVALID_DATA", 
+            error_description: "Os dados fornecidos no corpo da requisição são inválidos"
+        });
+    }
 
-// const getListOfMeasurements = async(req: Request, res: Response) => {
-//   try {
+    const measurementExists = await Measurement.findById({ measure_uuid });
+    
+    if (!measurementExists) {
+        return res.status(404).json({
+            error_code: "MEASURE_NOT_FOUND", 
+            error_description: "Leitura não encontrada"
+        })
+    }
 
-//   } catch (err: any) {
-//     res
-//       .status(500)
-//       .json({ message: "Erro interno do servidor", error: err.message });
-//   }
-// };
+    if (measurementExists.has_confirmed == true) {
+        return res.status(409).json({
+            error_code: "CONFIRMATION_DUPLICATE", 
+            error_description: "Leitura do mês já confirmada"
+        })
+    }
+
+    measurementExists.measure_value = confirmed_value
+    measurementExists.has_confirmed = true;
+    await measurementExists.save();
+
+    res.status(200).json({ sucess: true })
+  } catch (err: any) {
+    res
+      .status(500)
+      .json({ message: "Erro interno do servidor", error: err.message });
+  }
+};
+
+const getListOfMeasurements = async(req: Request, res: Response) => {
+    const customer_code = req.params.id;
+    const measure_type = req.query.measure_type as string;
+  
+    try {
+        if (!customer_code) {
+            return res.status(400).json({
+              error_code: "INVALID_CUSTOMER_CODE",
+              error_description: "O código do cliente inválido",
+            });
+        }
+
+        let filter: any = { customer_code };
+
+        if (measure_type) {
+            const normalizedMeasureType = measure_type.toUpperCase();
+            const allowedMeasureTypes = ["WATER", "GAS"];
+
+            if (!allowedMeasureTypes.includes(normalizedMeasureType)) {
+                return res.status(400).json({
+                    error_code: "INVALID_TYPE",
+                    error_description: "Tipo de medição não permitida"
+                })
+            }
+            filter = { customer_code, normalizedMeasureType };
+        }
+
+        const measurementsList = await Measurement.find(filter);
+
+        if (measurementsList.length === 0) {
+            return res.status(404).json({
+              error_code: "MEASURES_NOT_FOUND",
+              error_description: "Nenhuma leitura encontrada",
+            });
+        }
+
+        res.status(200).json({customer_code , measures: measurementsList})
+    } catch (err: any) {
+        res
+        .status(500)
+        .json({ message: "Erro interno do servidor", error: err.message });
+    }
+};
 
 export {
     uploadMeasurement,
-    // confirmMeasurement,
-    // getListOfMeasurements,
+    confirmMeasurement,
+    getListOfMeasurements,
 };
